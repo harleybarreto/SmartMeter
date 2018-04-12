f12d2aa7599035c203810c88c59cdd65cf9b434e
harleybarreto <37058418+harleybarreto@users.noreply.github.com>
2018-04-11 21:26:39 -0300

    Código que será baixado no ESP, incompleto.
    Possui apenas as bibliotecas wifiManager e Thingspeak, falta ainda a função que receberá os dados via serial.

---
+++ WifiManager___Thingspeak.ino
@@ -0,0 +1,43 @@
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+
+#include "ThingSpeak.h"
+
+unsigned long myChannelNumber = 31461;
+const char * myWriteAPIKey = "LD79EOAAWRVYF04Y";
+
+
+void setup() {
+
+    Serial.begin(115200);
+       
+    WiFiManager wifiManager;    
+    //reset saved settings
+    //wifiManager.resetSettings();    
+    //set custom ip for portal
+    //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+    wifiManager.autoConnect();
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+
+    ThingSpeak.begin(wifiManager);
+
+
+    
+}
+
+void loop() {
+  
+
+
+  
+  ThingSpeak.writeField(myChannelNumber, 1, voltage, myWriteAPIKey);
+  delay(20000); // ThingSpeak will only accept updates every 15 seconds.
+
+
+    
+}
